39af572b9303842bf8603e2473ff333fd92eff13
austin.lance@smoothstack.com <77307519+OldSociety@users.noreply.github.com>
2024-04-08 16:34:38 -0700

    message test threshold increase

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ global.messageCounter = 0
 // The number of messages to wait before sending a random message
 // let messageThreshold = Math.floor(Math.random() * (25 - 15 + 1)) + 15
 
-let messageThreshold = 18
+let messageThreshold = 54
 global.isQuestionActive = false
 
 // Listen for new messages
